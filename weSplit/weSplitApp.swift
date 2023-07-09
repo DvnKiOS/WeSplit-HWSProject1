8c75d882c2580a11ef201980b09f65e5ed006c5e
Devin King <DvnKiOS@gmail.com>
2023-07-08 23:23:49 -0400

    Initial Commit

---
+++ weSplit/weSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  weSplitApp.swift
+//  weSplit
+//
+//  Created by Devin King on 7/8/23.
+//
+
+import SwiftUI
+
+@main
+struct weSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
