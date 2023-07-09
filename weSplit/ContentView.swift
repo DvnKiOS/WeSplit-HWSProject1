ff5c83253bfc00161dbe8035f21f9d2119fb8a67
Devin King <DvnKiOS@gmail.com>
2023-07-09 12:54:40 -0400

    completed Challenges

--- weSplit/ContentView.swift
+++ weSplit/ContentView.swift
@@ -2,20 +2,99 @@
 //  ContentView.swift
 //  weSplit
 //
-//  Created by Devin King on 7/8/23.
+//  Created by Devin King on 7/8/21.
 //
 
-import SwiftUI
+import SwiftUI // tells swift to import the swiftUI Framework
 
-struct ContentView: View {
+struct ContentView: View { // -> Creates a new structure called contentview that is going to be forced to conform to the view protocol. View comes from SwiftUI and is needed if you want to put anything on screen.
+    @State private var checkAmount = 0.0
+    @State private var numberOfPeople = 2
+    @State private var tipPercentage = 20
+    
+    @FocusState private var amountIsFocused: Bool
+    
+    let tipPercentages = [10, 15, 20, 25, 0]
+    
+    var totalPerPerson: Double {
+        let peopleCount = Double(numberOfPeople + 2)
+        let tipSelection = Double(tipPercentage)
+        
+        let finalTipvalue = checkAmount / 100 * tipSelection
+        let grandTotal = checkAmount + finalTipvalue
+        let amountPerPerson = grandTotal / peopleCount
+        return amountPerPerson
+    }
+    var checkTotal: Double {
+        let checkPrice = checkAmount
+        let tipAmount = Double(tipPercentage)
+        let finalcheck = checkPrice + tipAmount
+        return finalcheck
+    }
+    
+    var currentLocation: FloatingPointFormatStyle<Double>.Currency {
+       
+        
+        return .currency(code: Locale.current.currency?.identifier ?? "USD")
+    }
+    
+    //since we want this value to be able to hold a vlaue we are going to mark it with state. a property wrapper that is going to allow us to hold a value within this particilar variable.
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+            Form {
+                Section {
+                    TextField("Amount", value: $checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                    .keyboardType(.decimalPad)
+                    .focused($amountIsFocused)
+                    
+                    Picker("Number of people", selection:  $numberOfPeople) {
+                        ForEach (2..<100) {
+                            Text("\($0) people")
+                        }
+                        
+                    }
+                    
+                    
+                }
+                Section {
+                    Picker("Tip Percentage", selection:  $tipPercentage) {
+                        ForEach(0..<101, id: \.self) {
+                            Text($0, format: .percent)
+                        }
+                    }
+                
+                } header: {
+                    Text("How much tip do you want to leave")
+                    
+                        
+                }
+                
+                Section {
+                    Text(checkTotal, format: .currency(code: Locale.current.identifier))
+                } header: {
+                    Text("Grand Total")
+                }
+                Section {
+                    Text(totalPerPerson, format: .currency(code: Locale.current.identifier ))
+                } header: {
+                    Text("Amount Per Person")
+                }
+            }
+            .navigationTitle("WeSplit")
+            .toolbar{
+                ToolbarItemGroup(placement: .keyboard) {
+                    Spacer()
+                    
+                    
+                    Button("Done") {
+                        amountIsFocused = false
+                    }
+                }
+            }
         }
-        .padding()
+        
+   
+        
     }
 }
 
